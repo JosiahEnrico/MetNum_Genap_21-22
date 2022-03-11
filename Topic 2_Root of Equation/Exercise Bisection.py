36f66ac0589baaba788f45414669a8c4186302c2
Bulldodger <74318244+JosiahEnrico@users.noreply.github.com>
2022-03-11 18:47:16 +0700

    Add files via upload

---
+++ Topic 2_Root of Equation/Exercise Bisection.py
@@ -0,0 +1,52 @@
+# Defining Function
+from tkinter.tix import INTEGER
+
+
+def f(x):
+    return x**3-5*x-9
+
+# Implementing Bisection Method
+def bisection(xa,xb,e,N):
+    err = 1
+    for step in range(1,N):
+        xt = (xa + xb)/2
+        print('Iteration-%d, x2 = %0.6f and err = %0.6f' % (step, xt, err))
+        err = abs(1 - xa/xt)
+        if err < e:
+            break
+        if f(xa)*f(xt) < 0:
+            xb = xt
+        else:
+            xa = xt
+
+    if err > e:
+        print('\nNot Convergent')
+    else:
+        print('\nRequired root is: %0.8f' % xt)
+    return   
+
+
+# Input Section
+xa = input('First Guess: ')
+xb = input('Second Guess: ')
+e = input('Tolerable Error: ')
+N = input('Max Iteration: ')
+
+# Converting input to float
+xa = float(xa)
+xb = float(xb)
+e = float(e)
+N = int(N)
+
+#Note: You can combine above two section like this
+# x0 = float(input('First Guess: '))
+# x1 = float(input('Second Guess: '))
+# e = float(input('Tolerable Error: '))
+# N = int(input('Max Iteration: '))
+
+# Checking Correctness of initial guess values and bisecting
+if f(xa) * f(xb) > 0.0:
+    print('Given guess values do not bracket the root.')
+    print('Try Again with different guess values.')
+else:
+    bisection(xa,xb,e,N)
